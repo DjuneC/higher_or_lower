0f353d65dc7c5daffb835b669f6b29dc031bd59e
Rodelin J Cesar <cesarrodelin@outlook.com>
2026-01-16 20:55:22 -0800

    Add data file

---
+++ src/main.py
@@ -0,0 +1,11 @@
+message1 = "Test1"
+message2 = "Test2"
+
+
+print("Higher or lower")
+print(f"Compare A: {message1}")
+
+print("Vs.")
+print(f"Against B: {message2}")
+
+answer = input("Who has more followers? Type 'A' or 'B': ")
