539ea190a4350bb0816e91f5435336365c02a238
Rodelin J Cesar <cesarrodelin@outlook.com>
2026-01-16 22:29:38 -0800

    Add logic to handle the higher behind the scene

--- src/game_data.py
+++ src/game_data.py
@@ -2,67 +2,67 @@ instagram_data = [
     {
         "name": "Cristiano Ronaldo",
         "follower_count": 60600000,
-        "description": "Footballer and entrepreneur.",
+        "description": "Footballer and entrepreneur",
         "country": "Portugal"
     },
     {
         "name": "Lionel Messi",
         "follower_count": 48700000,
-        "description": "Argentine football superstar.",
+        "description": "Argentine football superstar",
         "country": "Argentina"
     },
     {
         "name": "Selena Gomez",
         "follower_count": 43000000,
-        "description": "Singer, actress, and producer.",
+        "description": "Singer, actress, and producer",
         "country": "United States"
     },
     {
         "name": "Taylor Swift",
         "follower_count": 28000000,
-        "description": "Singer-songwriter.",
+        "description": "Singer-songwriter",
         "country": "United States"
     },
     {
         "name": "Nike",
         "follower_count": 25300000,
-        "description": "Global leader in sport footwear, apparel, equipment, and accessories.",
+        "description": "Global leader in sport footwear, apparel, equipment, and accessories",
         "country": "United States"
     },
     {
         "name": "Adidas",
         "follower_count": 23000000,
-        "description": "German multinational corporation that designs, develops, manufactures, and distributes sports shoes, clothing, and accessories.",
+        "description": "German multinational corporation that designs, develops, manufactures, and distributes sports shoes, clothing, and accessories",
         "country": "Germany"
     },
     {
         "name": "Victoria Secret",
         "follower_count": 12800000,
-        "description": "American lingerie company.",
+        "description": "American lingerie company",
         "country": "United States"
     },
     {
         "name": "Coca-Cola",
         "follower_count": 38100000,
-        "description": "American beverage company.",
+        "description": "American beverage company",
         "country": "United States"
     },
     {
         "name": "Samsung",
         "follower_count": 22800000,
-        "description": "South Korean electronics company.",
+        "description": "South Korean electronics company",
         "country": "South Korea"
     },
     {
         "name": "Justin Bieber",
         "follower_count": 118000000,
-        "description": "Canadian singer and songwriter.",
+        "description": "Canadian singer and songwriter",
         "country": "Canada"
     },
     {
         "name": "Netflix",
         "follower_count": 25900000,
-        "description": "American streaming service.",
+        "description": "American streaming service",
         "country": "United States"
     }
 ]
--- src/main.py
+++ src/main.py
@@ -1,10 +1,34 @@
+import random
+
 from game_data import instagram_data
 
 
 def get_random_data():
+    data = random.choice(instagram_data)
+
+    return {"name": data["name"], "description": data["description"], "country": data["country"], "follower_count": data["follower_count"]}
+
+def display_data(data_to_display):
     pass
 
 def main():
-    pass
+    first = get_random_data()
+    second = get_random_data()
+    f_follower_count = first.get("follower_count")
+    s_follower_count = second.get("follower_count")
 
-answer = input("Who has more followers? Type 'A' or 'B': ")
+    print("Higher or lower")
+
+    print(f"Compare A: {first.get("name")}, a {first.get("description")}, from {first.get("country")}")
+    print("Vs.")
+    print(f"Against B: {second.get("name")}, a {second.get("description")}, from {second.get("country")}")
+
+    answer = input("Who has more followers? Type 'A' or 'B': ").lower()
+
+    result = "A" if f_follower_count > s_follower_count else "B"
+
+    print(result)
+
+    
+if __name__ == "__main__":
+    main()
