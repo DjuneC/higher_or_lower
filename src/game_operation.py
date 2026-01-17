a758bdc4d2d122cb36746b8d750d8e3d741459ac
Rodelin J Cesar <cesarrodelin@outlook.com>
2026-01-17 01:06:18 -0800

    Implement v1 final code

--- src/game_operation.py
+++ src/game_operation.py
@@ -8,6 +8,9 @@ from game_data import instagram_data
 def clear_console():
     os.system("cls" if os.name == "nt" else "clear")
 
+def get_follower_count(first, second):
+    return first.get("follower_count"), second.get("follower_count")
+
 def get_random_data():
     data = random.choice(instagram_data)
 
@@ -23,5 +26,34 @@ def display_data(first, second, score=0):
     print(logo2)
     print(f"Against B: {second.get("name")}, a {second.get("description")}, from {second.get("country")}")
 
-def get_follower_count(first, second):
-    return first.get("follower_count"), second.get("follower_count")
+def play(first_value, second_value, score):
+
+    display_data(first=first_value, second=second_value)
+
+    while True:
+
+        answer = input("Who has more followers? Type 'A' or 'B': ").upper().strip()
+        
+        if answer not in ["A", "B"]:
+            print("Can you read? look again. (Choose A or B)")
+            continue
+
+        f_follower_count, s_follower_count = get_follower_count(first=first_value, second=second_value)
+        result = "A" if f_follower_count > s_follower_count else "B"
+
+
+        if answer != result:
+            clear_console()
+            print(logo1)
+            print(f"Sorry, that's wrong. Final score: {score}")
+            break
+
+        score += 1
+
+        if result != "A":
+            first_value = second_value
+
+        second_value = get_random_data()
+        
+        clear_console()
+        display_data(first=first_value, second=second_value, score=score)
--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from art import logo1
-from game_operation import get_follower_count, get_random_data, clear_console, display_data
+from game_operation import play, get_follower_count, get_random_data, clear_console, display_data
 
 
 def main():
@@ -8,39 +8,8 @@ def main():
 
     score = 0
 
-    display_data(first=first, second=second)
-
-    while True:
-
-        answer = input("Who has more followers? Type 'A' or 'B': ").upper().strip()
-
-        result = "A" if f_follower_count > s_follower_count else "B"
-        print(result)
-
-        if answer not in ["A", "B"]:
-            print("Can you read? look again. (Choose A or B)")
-            continue
-
-        f_follower_count, s_follower_count = get_follower_count(first=first, second=second)
-
-        if answer != result:
-            clear_console()
-            print(logo1)
-            print(f"Sorry, that's wrong. Final score: {score}")
-            break
-
-        score += 1
-
-        if result != "A":
-            first = second
-
-        second = get_random_data()
-        
-        clear_console()
-        display_data(first=first, second=second, score=score)
-        
-    
-
+    play(first_value=first, second_value=second, score=score)        
+   
     
 if __name__ == "__main__":
     main()
